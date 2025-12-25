ea21b4fda72d224ffd2be7bdd75c3ffe9cb96133
Mustafa Barakzey <mustafabarakzey@gmail.com>
2025-12-25 17:09:54 +0330

    throw on user model when email is not valid

--- src/Domain/User/Entities/UserModel.cs
+++ src/Domain/User/Entities/UserModel.cs
@@ -1,3 +1,5 @@
+using brk.Todo.Domain.Shared.Exceptions;
+
 namespace brk.Todo.Domain.User.Entities;
 
 public class UserModel
@@ -8,6 +10,9 @@ public class UserModel
     private UserModel(){}
     public static UserModel Register(string email, string password)
     {
+        if (!email.Contains('@'))
+            throw new DomainException("email is not valid");
+            
         var user = new UserModel();
         user.Email = email;
         user.Password = password;
--- tests/UnitTests/User/Entities/UserModelTest.cs
+++ tests/UnitTests/User/Entities/UserModelTest.cs
@@ -1,5 +1,4 @@
-
-
+using brk.Todo.Domain.Shared.Exceptions;
 using brk.Todo.Domain.User.Entities;
 
 namespace brk.Todo.UnitTests.User.Entities;
@@ -18,5 +17,19 @@ public class UserModelTest
         // Then
         Assert.Equal(email, user.Email);
         Assert.Equal(password, user.Password);
+    }
+    
+    [Fact]
+    public void Register_Should_Throw_EmailIsNotValid()
+    {
+        // Given
+        var email = "mustafabarakzeygmail.com";
+        var password = "MyStr0ngP@ss";
+
+        // When
+        var action =()=> UserModel.Register(email, password);
+        
+        // Then
+        Assert.Throws<DomainException>(action);
     }
 }
